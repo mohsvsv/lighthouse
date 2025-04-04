f3c6c398c834951845913edd60a6fde26b3264a2
Adam Raine <ad.st.raine@gmail.com>
2025-04-03 17:08:06 -0700

    ope

--- core/test/devtools-tests/run-e2e-tests.sh
+++ core/test/devtools-tests/run-e2e-tests.sh
@@ -15,4 +15,4 @@ export LH_ROOT="$SCRIPT_DIR/../../.."
 cd "$DEVTOOLS_PATH"
 
 TEST_PATTERN="${1:-test/e2e/lighthouse/*}"
-npm run test -- "$TEST_PATTERN" --t $BUILD_FOLDER
+npm run test -- "$TEST_PATTERN" -t $BUILD_FOLDER
